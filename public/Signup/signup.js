1607e5fc0696a6d858b8365a429a55b0b07e95b1
AkashB07 <akashbadhi7@gmail.com>
2022-11-29 14:08:17 +0530

    Integrating frontend with backend

--- public/Signup/signup.js
+++ public/Signup/signup.js
@@ -14,18 +14,19 @@ async function signup(e)
         // console.log(signupDetails.email);
 
         const respone = await axios.post(`${url}:3000/user/signup`, signupDetails)
-        console.log(respone.succes);
+        // console.log(respone.succes);
         if(respone.succes)
         {
+            alert("Successfuly signed up");
             window.location.href ="../Login/login.html";
         }
         else
         {
-            throw new Error(respone.message)
+            throw new Error(respone.message);
         }
     } 
     catch (error) 
     {
-        console.log(error);
+        alert(error);
     }
 }
