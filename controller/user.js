3457e6add75f33abe0c72630bc617683e03bbf6e
AkashB07 <akashbadhi7@gmail.com>
2022-11-29 16:25:22 +0530

    login backend

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,6 @@
 const User = require('../modles/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 function isstringinvalid(string){
     if(string == undefined || string.length === 0){
@@ -37,6 +38,44 @@ const signup = async (req, res) => {
     }
 }
 
+function generateAccessToken(id, name){
+    return jwt.sign({userId: id, name: name}, process.env.TOKEN_SECRET)
+}
+
+const login = async (req, res) => {
+    try 
+    {
+        const {email, password} = req.body;
+        if(isstringinvalid(email) || isstringinvalid(password))
+        {
+            return res.status(400).json({message: 'Email id or password is missing', success: false});
+        }
+        const user = await User.findAll({ where : { email }})
+        // console.log(user[0])
+        if(user.length > 0){
+            bcrypt.compare(password, user[0].password, (err, result) => {
+                if(err){
+                    throw new Error('Something went wrong')
+                }
+                if(result){
+                    // console.log('123')
+                    return res.status(200).json({message: "User logged in successfully", token: generateAccessToken(user[0].id, user[0].name), success: true})
+                }
+                else{
+                    return  res.status(401).json({message: 'Password is incorrect', success: false});
+                }
+            })
+        }
+        else{
+            return  res.status(404).json({message: 'User does not exist', success: false});
+        }
+    } 
+    catch (err) {
+        res.status(500).json({message: err, success: false});
+    }
+}
+
 module.exports = {
-    signup
+    signup,
+    login
 }
--- public/Login/login.js
+++ public/Login/login.js
@@ -9,13 +9,13 @@ async function login(e)
             email: e.target.email.value,
             password: e.target.password.value
         }
-        // console.log(loginDetails);
-
-        const respone = await axios.post(`${url}:3000/user/login`, loginDetails)
+        const respone = await axios.post(`${url}:3000/user/login`, loginDetails);
+        // console.log(respone.token);
+        localStorage.setItem('token', respone.token)
+        alert(respone.message);
     }
     catch (err) 
     {
-        // console.log(JSON.stringify(err))
-        document.body.innerHTML += `<div style="color:red;">${err.message}<div>`;
+        alert(err.message);
     }
 }
