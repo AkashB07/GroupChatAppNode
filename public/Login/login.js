d8354110cf01de8e876ec64d3488e69aaffc40ac
AkashB07 <akashbadhi7@gmail.com>
2022-11-30 13:50:12 +0530

    chat application frontend

--- public/Login/login.js
+++ public/Login/login.js
@@ -10,9 +10,12 @@ async function login(e)
             password: e.target.password.value
         }
         const respone = await axios.post(`${url}:3000/user/login`, loginDetails);
-        // console.log(respone.token);
-        localStorage.setItem('token', respone.token)
+        // console.log(respone.name);
+        localStorage.setItem('token', respone.token);
+        localStorage.setItem('name', respone.name);
+        localStorage.setItem('email', respone.email);
         alert(respone.message);
+        window.location.href ='../Home/home.html';
     }
     catch (err) 
     {
